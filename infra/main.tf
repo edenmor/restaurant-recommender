303b366c823c797988d8436b9eddc5f8596409a5
eden <eden@edens-MacBook-Pro.local>
2024-12-29 01:30:20 +0200

    changed terraform version

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
 provider "azurerm" {
   features {}
 }
